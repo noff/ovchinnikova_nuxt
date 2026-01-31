72b9bbacf2ee9ec9003fd1da30a55570054dae0f
Michael Kechinov <kechinoff@gmail.com>
2026-01-31 20:29:48 +0300

    add hrustalnye goroda, remove brick

---
+++ app/pages/book_hrustalnye_goroda.vue
@@ -0,0 +1,49 @@
+<template>
+  <section>
+    <PageTitle title="Хрустальные города" second-url="/books" second-label="Книги" />
+
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-4">
+          <div class="mb-3">
+            <img src="/images/books/book-hrustalnye-goroda@2x.jpg" alt="Хрустальные города" class="img-fluid book-img" />
+          </div>
+          <div>ISBN: 978-5-08-007517-9</div>
+          <div>Издательство: Детская литература, 2025 г.</div>
+          <hr class="my-4 py-0">
+          <h4>Купить бумажную книгу</h4>
+          <div class="d-flex justify-content-between py-2">
+            <a href="https://detlit.ru/lmk-ovchinnikova-hrustalnye-goroda/" target="_blank">Сайт издательства</a>
+          </div>
+          <div class="d-flex justify-content-between py-2">
+            <a href="https://www.wildberries.ru/catalog/771153833/detail.aspx" target="_blank">WB</a>
+          </div>
+
+          <hr class="my-4 py-0">
+        </div>
+
+        <div class="col-md-8">
+
+          <p>Мне всегда хотелось написать о первой любви, именно как она начинается - с первого взгляда, улыбки, с первого движения друг к другу и до пожара, в котором горят герой и героиня.</p>
+          <p>Так получилось, что я созрела для темы в 2022-м году одновременно с тем, как активно вовлеклась в волонтерскую тему. Наша небольшая группа помогает жителям прифронтовых районов, потому что там, как понимаете, остаются самые незащищенные.</p>
+          <p>Поэтому повесть получилась сплавом того, что я хотела рассказать о любви, о беженцах и волонтерах, об отголосках СВО. Но в целом, как и все мои тексты, он прежде всего - о людях, о силе жизни, о характерах и судьбах.</p>
+          <p>И - да, волонтерский склад в Казанском соборе на самом деле существует.</p>
+
+        </div>
+      </div>
+    </div>
+
+  </section>
+</template>
+
+<script setup lang="ts">
+import Review from "~/components/Review.vue";
+
+useHead({
+  title: 'Хрустальные города – Евгения Овчинникова, писательница',
+  meta: [
+    { name: 'description', content: 'Мне всегда хотелось написать о первой любви, именно как она начинается - с первого взгляда, улыбки, с первого движения друг к другу и до пожара, в котором горят герой и героиня. ' },
+  ],
+})
+</script>
